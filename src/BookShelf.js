1cccba11d78389a45561700a74d7a49af88abf66
Peter Ambruzs <ambruzsp@gmail.com>
2018-02-26 21:50:43 +0100

    feat: add animation to bookshelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 import Book from './Book';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const Fade = ({ children, ...props }) => (
+  <CSSTransition
+    {...props}
+    timeout={1000}
+    classNames="fade"
+  >
+    {children}
+  </CSSTransition>
+);
 
 class BookShelf extends Component {
   render() {
@@ -7,15 +18,19 @@ class BookShelf extends Component {
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
+          <ol>
+          <TransitionGroup className='books-grid'>
             {this.props.books.map((book) => (
-              <li key={book.id}>
+              <Fade key={book.id}>
+              <li>
                 <Book
                   book={book}
                   onUpdateBook={this.props.onUpdateBook}
                 />
               </li>
+              </Fade>
             ))}
+            </TransitionGroup>
           </ol>
         </div>
       </div>
